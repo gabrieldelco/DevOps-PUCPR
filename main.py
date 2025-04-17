f456de179322eb3cb69c9f45b09e8e23b7d94f04
gabrieldelco <gabrieldelconti99@gmail.com>
2025-04-17 14:09:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+import random
 app = FastAPI()
 
 @app.get("/")
@@ -8,4 +8,4 @@ async def root():
 
 @app.get("/teste1")
 async def funcaoteste():
-    return{"teste": "deu certo"}
+    return{"teste": True, "num_aleatorio": random.randint(0, 1000)}
